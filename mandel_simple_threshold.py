a7d1d9af9fdb583370b2182db6f6f57680aa339b
Grégory Chatel <chatel.gregory@gmail.com>
2015-06-20 16:00:35 +0200

    Adding the code for the thresholding.

--- mandel_simple_threshold.py
+++ mandel_simple_threshold.py
@@ -10,7 +10,8 @@ maxIter = 100
 width = 1800
 height = 1200
 
-def draw_mandel(window, sequence_function):
+def draw_mandel(window, sequence_function, width, height, max_iter,
+                threshold):
     x_ratio = 3. / width
     y_ratio = 2. / height
     for x in xrange(width):
@@ -21,18 +22,27 @@ def draw_mandel(window, sequence_function):
                 z = sequence_function(z, c)
                 if (z.real * z.real + z.imag * z.imag) > 4:
                     color_ratio = int((i * 255.) / maxIter)
-                    gfxdraw.pixel(window, x, y, Color(color_ratio, 0, 0, 255))
+                    if color_ratio >= threshold:
+                        gfxdraw.pixel(window, x, y, Color(0, 0, 0, 255))
+                    else:
+                        gfxdraw.pixel(window, x, y, Color(255, 255, 255, 255))
                     break
             else:
-                gfxdraw.pixel(window, x, y, Color(0,0,0,255))
+                gfxdraw.pixel(window, x, y, Color(255,255,255,255))
 
     pygame.display.flip()
-    pygame.image.save(window, 'render.png')
+    pygame.image.save(window, 'render_border.png')
 
 if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        print "width, height, max_iter, threshold"
+        sys.exit(1)
+
+    width, height, max_iter, threshold = [int(x) for x in sys.argv[1:]]
     window = pygame.display.set_mode((width, height))
 
-    draw_mandel(window, lambda z, c: z**2 + c)
+    draw_mandel(window, lambda z, c: z**2 + c, width, height,
+                max_iter, threshold)
 
     while True:
         for event in pygame.event.get():
